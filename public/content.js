6d455a89ba7d8115d18923e2a545ea38b0023fc7
daniman123 <>
2023-11-22 01:52:57 +0100

    suiiiiiiiiiiiiii

--- public/content.js
+++ public/content.js
@@ -1,26 +1,71 @@
 function simulateClickOnElement(selector) {
-	const element = document.querySelector(selector);
+  const element = document.querySelector(selector);
 
-	if (element) {
-		["mouseover", "mousedown", "mouseup", "click"].forEach((eventType) => {
-			element.dispatchEvent(
-				new MouseEvent(eventType, {
-					bubbles: true,
-					cancelable: true,
-					view: window,
-				})
-			);
-		});
-	} else {
-		console.log("Element not found: " + selector);
-	}
+  if (element) {
+    ["mouseover", "mousedown", "mouseup", "click"].forEach((eventType) => {
+      element.dispatchEvent(
+        new MouseEvent(eventType, {
+          bubbles: true,
+          cancelable: true,
+          view: window,
+        })
+      );
+    });
+  } else {
+    console.log("Element not found: " + selector);
+  }
+}
+
+export const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export function typingDelay() {
+  // Average typing speed is between 30-60 WPM.
+  // This translates to approximately 100 - 300 milliseconds per character.
+  return Math.random() * (300 - 100) + 100;
+}
+
+export async function simulateTyping(element, text, typingDelay) {
+  for (const char of text) {
+    element.value += char;
+    await delay(typingDelay());
+  }
+}
+
+export async function simulateTypingWithTypos(element, text, typingDelay) {
+  for (let i = 0; i < text.length; i++) {
+    let char = text[i];
+
+    // Randomly decide to make a typo
+    if (Math.random() < 0.1) {
+      // 10% chance of typo
+      char = getRandomChar();
+      element.value += char;
+      await delay(typingDelay());
+
+      // Correct the typo
+      element.value = element.value.slice(0, -1);
+      await delay(typingDelay());
+    }
+
+    // Type the correct character
+    element.value += char;
+    await delay(typingDelay());
+  }
+}
+
+export function getRandomChar() {
+  const chars = "abcdefghijklmnopqrstuvwxyz";
+  return chars[Math.floor(Math.random() * chars.length)];
 }
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-	if (request.action === "simulateClick") {
-		console.log("content file message recievde");
-		const selector = request.selector;
-		console.log("🚀 ~ file: content.js:23 ~ chrome.runtime.onMessage.addListener ~ selector:", selector)
-		simulateClickOnElement(selector);
-	}
+  if (request.action === "simulateClick") {
+    console.log("content file message recievde");
+    const selector = request.selector;
+    console.log(
+      "🚀 ~ file: content.js:23 ~ chrome.runtime.onMessage.addListener ~ selector:",
+      selector
+    );
+    simulateClickOnElement(selector);
+  }
 });
--- src/utils.js
+++ src/utils.js
@@ -1,48 +1,49 @@
 import selectors from "./selectors/selectors.json";
 
 export const executeScriptOnActiveTab = (tabId, scriptDetails, logMessage) => {
-	chrome.scripting
-		.executeScript({
-			target: { tabId },
-			...scriptDetails,
-		})
-		.then(() => console.log(logMessage));
+  chrome.scripting
+    .executeScript({
+      target: { tabId },
+      ...scriptDetails,
+    })
+    .then(() => console.log(logMessage));
 };
 
 export const getSelector = (
-	selectors,
-	selectorPrefix,
-	selectorType,
-	uniqueSelector,
-	seperator = ".",
-	baseKey = "base"
+  selectors,
+  selectorPrefix,
+  selectorType,
+  uniqueSelector,
+  seperator = ".",
+  baseKey = "base"
 ) => {
-	const base = selectors[0][selectorType]["shared"][baseKey];
-	const unique = selectors[0][selectorType]["unique"][uniqueSelector];
+  const base = selectors[0][selectorType]["shared"][baseKey];
+  const unique = selectors[0][selectorType]["unique"][uniqueSelector];
 
-	const selectorComponents = [base, unique];
+  const selectorComponents = [base, unique];
 
-	const selector = selectorComponents.join(seperator);
+  const selector = selectorComponents.join(seperator);
 
-	return selectorPrefix + selector;
+  return selectorPrefix + selector;
 };
 
 export const simulateClick = (
-	selectorPrefix,
-	selectorType,
-	uniqueSelector,
-	seperator="."
+  selectorPrefix,
+  selectorType,
+  uniqueSelector,
+  seperator = "."
 ) => {
-	chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-		chrome.tabs.sendMessage(tabs[0].id, {
-			action: "simulateClick",
-			selector: `${getSelector(
-				selectors,
-				selectorPrefix,
-				selectorType,
-				uniqueSelector,
-				seperator
-			)}`,
-		});
-	});
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, {
+      action: "simulateClick",
+      selector: `${getSelector(
+        selectors,
+        selectorPrefix,
+        selectorType,
+        uniqueSelector,
+        seperator
+      )}`,
+    });
+  });
 };
+
