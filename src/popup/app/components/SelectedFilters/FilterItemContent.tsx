0a908e02dd1eee0a1905a01f81524d74a7b4b13c
daniman123 <daniel-saleh@hotmail.com>
2023-12-16 20:15:46 +0100

    display of price fileters

--- src/popup/app/components/SelectedFilters/FilterItemContent.tsx
+++ src/popup/app/components/SelectedFilters/FilterItemContent.tsx
@@ -1,6 +1,29 @@
 import { IFilterItemControls } from "../../types";
 import { FilterItemControls } from "./FilterItemControls";
 
+const DisplayPriceFilters = ({ priceFilters }: { priceFilters: any }) => {
+  const minBid = priceFilters[0];
+  const maxBid = priceFilters[1];
+  const minBin = priceFilters[2];
+  const maxBin = priceFilters[3];
+
+  const divStyle = "flex gap-1 border";
+
+  return (
+    <>
+      Price Filters
+      <div className={divStyle}>
+        <p>Min Bid: {minBid}</p>
+        <p>Max Bid: {maxBid}</p>
+      </div>
+      <div className={divStyle}>
+        <p>Min Bin: {minBin}</p>
+        <p>Max Bin: {maxBin}</p>
+      </div>
+    </>
+  );
+};
+
 export const FilterItemContent = (props: IFilterItemControls) => {
   return (
     <>
@@ -8,7 +31,7 @@ export const FilterItemContent = (props: IFilterItemControls) => {
         <FilterItemControls {...props} />
       </div>
       {props.element.rarityFilters}
-      {props.element.priceFilters.join(",")}
+      <DisplayPriceFilters {...props.element} />
     </>
   );
 };
